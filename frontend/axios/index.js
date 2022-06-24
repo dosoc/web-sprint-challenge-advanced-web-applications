bd97648d4ee5d582866ed3ed056f3e5045eb96cf
dosoc <18337634+dosoc@users.noreply.github.com~>
2022-06-23 21:16:32 -0500

    adds token variable to authorization

--- frontend/axios/index.js
+++ frontend/axios/index.js
@@ -3,6 +3,7 @@
 import axios from 'axios'
 
 const axiosWithAuth = () => {
+    const token = localStorage.getItem("token")
     return axios.create({
         headers: {
             authorization: token
