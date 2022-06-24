97bce15561c53037ccf8de034f19f0b3b8dd7dd2
dosoc <18337634+dosoc@users.noreply.github.com~>
2022-06-24 11:42:18 -0500

    adds spinner test

--- frontend/components/Spinner.test.js
+++ frontend/components/Spinner.test.js
@@ -1,5 +1,19 @@
 // Import the Spinner component into this file and test
 // that it renders what it should for the different props it can take.
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import Spinner from './Spinner'
+
 test('sanity', () => {
-  expect(true).toBe(false)
+  expect(true).toBe(true)
+})
+
+test('renders without error', ()=> {
+  render(<Spinner on={true}/>)
+})
+
+test('correct text appears on the screen', ()=> {
+  render(<Spinner on={true}/>)
+  const text = screen.queryByText(/please wait/i);
+  expect(text).toBeTruthy()
 })
